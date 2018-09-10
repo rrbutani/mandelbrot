6563eecbf79b029e8c4431bce14a005fa3df93bc
Rahul Butani <rr.butani@gmail.com>
2018-09-10 18:54:17 -0500

    Switched to a fork of `png` that supports encoding APNGs
    So, now we have a two animated examples (APNG and GIF - the GIF one is super slow because of the palette stuff)
    
    Also, the APNG spec is pretty snazzy

--- examples/animated.rs
+++ examples/animated.rs
@@ -1,13 +1,21 @@
 extern crate clap;
-extern crate gif;
+extern crate png;
 extern crate mandelbrot;
 
-use gif::SetParameter;
-use mandelbrot::mandelbrot::{Mandelbrot, MandelbrotConfig, Viewport};
-use mandelbrot::{pixel::{Pixel, PixelMath}, complex_number};
+use mandelbrot::mandelbrot::MandelbrotConfig;
+use mandelbrot::mandelbrot::Mandelbrot;
+use mandelbrot::mandelbrot::Viewport;
+use png::HasParameters;
 
 mod shared;
 use shared::{cli, common};
+use std::io::BufWriter;
+
+use mandelbrot::{pixel::{Pixel, PixelMath, IntoPixel}, complex_number};
+
+use mandelbrot::color_scale::SimpleColorScale;
+use mandelbrot::color_scale::ContinuousColorScale;
+use mandelbrot::color_scale::ColorScale;
 
 fn main() {
     let matches = cli::args().get_matches();
@@ -15,14 +23,13 @@ fn main() {
     let dimensions = cli::get_dimensions(&matches).expect("Invalid dimensions");
     let (w, h) = dimensions;
     let f = cli::get_number_of_frames(&matches).expect("Invalid number of frames");
-    let mut file = cli::get_output_file(&matches, "a.gif").expect("Couldn't create file");
+    let file = cli::get_output_file(&matches, "a.png").expect("Couldn't create file");
 
-    // println!("{:?}, {:?}", w, h);
-    // println!("{:?}", f);
+    let ref mut buf = BufWriter::new(file);
 
-    let mut encoder = gif::Encoder::new(&mut file, w as u16, h as u16, &[]).unwrap();
-
-    encoder.set(gif::Repeat::Infinite).unwrap();
+    let mut encoder = png::Encoder::new_animated(buf, w, h, f).unwrap();
+    encoder.set(png::ColorType::RGBA).set(png::BitDepth::Eight);
+    let mut writer = encoder.write_header().unwrap();
 
     let viewport = Viewport::<f64> {
         top_left: complex_number::ComplexNumber::new(-3.0, 1.15),
@@ -33,41 +40,60 @@ fn main() {
     let config = MandelbrotConfig::<u8> {
         dimensions: dimensions,
         viewport: viewport,
-        _pixel: Pixel::<u8>::default(),
+        color_fn: ContinuousColorScale::pixel_color,
+    };
+
+    let mut mandelbrot = Mandelbrot::new(config);
+    mandelbrot.run_iterations(f-1);
+
+    writer.write_frame(common::flatten_array(mandelbrot.get_pixels()).as_slice()).unwrap();
+
+    let viewport = Viewport::<f64> {
+        top_left: complex_number::ComplexNumber::new(-3.0, 1.15),
+        width: 4f64,
+        height: (h as f64 / w as f64) * 4f64,
+    };
+
+    let config = MandelbrotConfig::<u8> {
+        dimensions: dimensions,
+        viewport: viewport,
+        color_fn: ContinuousColorScale::pixel_color,
     };
 
     let mut mandelbrot = Mandelbrot::new(config);
 
     println!("Running {} iterations", f);
 
-    for _i in 0..f {
+    for _i in 0..f-1 {
         mandelbrot.run_iterations(1);
-        println!("Ran an iteration");
+        let data = common::flatten_array(mandelbrot.get_pixels());
+        let data = data.as_slice();
 
-        let pixels = mandelbrot.get_pixels();
-        println!("Got the pixels");
-
-        let frame = gif::Frame::from_rgba(w as u16, h as u16, &mut common::flatten_array(pixels));
-
-        println!("Made a frame");
-    
-        encoder.write_frame(&frame).unwrap();
-    
-        println!("Finished writing the frame!");
+        writer.write_frame(data).unwrap();
     }
 
-    // mandelbrot.run_iterations(f);
+    // mandelbrot.run_iterations(50);
 
     // let data = mandelbrot.get_pixels();
 
-    // // let mut data = vec![vec![vec![ 0 as u8; 4]; w as usize]; h as usize];
+    // let mut data = vec![vec![vec![ 0 as u8; 4]; w as usize]; h as usize];
 
-    // // data[(w - w) as usize][(h - h) as usize][0] = 255;
-    // // data[(w - w) as usize][(h - h) as usize][1] = 255;
-    // // data[(w - w) as usize][(h - h) as usize][2] = 0;
-    // // data[(w - w) as usize][(h - h) as usize][3] = 255;
+    // data[(w - w) as usize][(h - h) as usize][0] = 255;
+    // data[(w - w) as usize][(h - h) as usize][1] = 255;
+    // data[(w - w) as usize][(h - h) as usize][2] = 0;
+    // data[(w - w) as usize][(h - h) as usize][3] = 255;
 
 
 
     // writer.write_image_data(common::flatten_array(data).as_slice()).unwrap();
+
+    // println!("{:?}, {:?}", w, h);
+
+    // let px = Pixel::new(9u8, 234, 5);
+
+    // let iter = IntoPixel::new(&px);
+
+    // for i in iter {
+    //     println!("{:?}", i);
+    // }
 }
---
+++ examples/animated_gif.rs
@@ -0,0 +1,73 @@
+extern crate clap;
+extern crate gif;
+extern crate mandelbrot;
+
+use gif::SetParameter;
+use mandelbrot::mandelbrot::{Mandelbrot, MandelbrotConfig, Viewport};
+use mandelbrot::complex_number;
+use mandelbrot::color_scale::ContinuousColorScale;
+use mandelbrot::color_scale::ColorScale;
+
+mod shared;
+use shared::{cli, common};
+
+fn main() {
+    let matches = cli::args().get_matches();
+
+    let dimensions = cli::get_dimensions(&matches).expect("Invalid dimensions");
+    let (w, h) = dimensions;
+    let f = cli::get_number_of_frames(&matches).expect("Invalid number of frames");
+    let mut file = cli::get_output_file(&matches, "a.gif").expect("Couldn't create file");
+
+    let mut encoder = gif::Encoder::new(&mut file, w as u16, h as u16, &[]).unwrap();
+
+    encoder.set(gif::Repeat::Infinite).unwrap();
+
+    let viewport = Viewport::<f64> {
+        top_left: complex_number::ComplexNumber::new(-3.0, 1.15),
+        width: 4f64,
+        height: (h as f64 / w as f64) * 4f64,
+    };
+
+    let config = MandelbrotConfig::<u8> {
+        dimensions: dimensions,
+        viewport: viewport,
+        color_fn: ContinuousColorScale::pixel_color,
+        // color_fn: SimpleColorScale::pixel_color,
+    };
+
+    let mut mandelbrot = Mandelbrot::new(config);
+
+    println!("Running {} iterations", f);
+
+    for _i in 0..f {
+        mandelbrot.run_iterations(1);
+        println!("Ran an iteration");
+
+        let pixels = mandelbrot.get_pixels();
+        println!("Got the pixels");
+
+        let frame = gif::Frame::from_rgba(w as u16, h as u16, &mut common::flatten_array(pixels));
+
+        println!("Made a frame");
+    
+        encoder.write_frame(&frame).unwrap();
+    
+        println!("Finished writing the frame!");
+    }
+
+    // mandelbrot.run_iterations(f);
+
+    // let data = mandelbrot.get_pixels();
+
+    // // let mut data = vec![vec![vec![ 0 as u8; 4]; w as usize]; h as usize];
+
+    // // data[(w - w) as usize][(h - h) as usize][0] = 255;
+    // // data[(w - w) as usize][(h - h) as usize][1] = 255;
+    // // data[(w - w) as usize][(h - h) as usize][2] = 0;
+    // // data[(w - w) as usize][(h - h) as usize][3] = 255;
+
+
+
+    // writer.write_image_data(common::flatten_array(data).as_slice()).unwrap();
+}
